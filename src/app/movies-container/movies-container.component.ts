bbdeea4e2ef4a0cf8dba352be61e6462fc351895
daibeat <daibeat@github.com>
2021-08-18 18:13:56 +1000

    navbar added

--- src/app/movies-container/movies-container.component.ts
+++ src/app/movies-container/movies-container.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-movies-container',
+  selector: 'movies-container',
   templateUrl: './movies-container.component.html',
   styleUrls: ['./movies-container.component.less']
 })
